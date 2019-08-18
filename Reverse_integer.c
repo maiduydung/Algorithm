056ad7edd6a89f0dd46c2f94235dd7f757122cac
Mai Duy Dũng <maiduydung@yahoo.com>
2019-08-18 21:20:37 +0700

    Update Reverse_integer.c

--- Reverse_integer.c
+++ Reverse_integer.c
@@ -1,5 +1,5 @@
 #include <stdint.h>
-#include <math.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,29 +7,21 @@
 
 int reverse (int x)
 {
-    int32_t my_32bit_int;
-    if (x != 0)
+    int result = 0;
+    int right_num;
+
+    while (x != 0)
     {
-        int nDigits = floor(log10(abs(x))) + 1;
-        char x_str[nDigits];
-        sprintf(x_str,"%d",x);
-       
-        printf("%s",x_str);
-
-        for (int i = strlen(x_str); i >= 0 ; i--)
-        {
-            printf("%c",x_str[i]);
-        }
-
-        //return nDigits;
+        right_num = x % 10;
+        x = x / 10;
+        result = result*10 + right_num;
     }
-    return 0;
+    return result;
+
 }
 
 int main(){
     
-    reverse(1234);
-    //printf("%d\n",reverse(1234));
-
+    printf("%d\n",reverse(1234));
     return 0;  
 }
