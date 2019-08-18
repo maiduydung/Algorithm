efc69b055a405039196f892b283dbd92018b092c
Mai Duy Dũng <maiduydung@yahoo.com>
2019-08-18 23:04:43 +0700

    Update Reverse_integer.c

--- Reverse_integer.c
+++ Reverse_integer.c
@@ -1,24 +1,25 @@
-#include <stdint.h>
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
+#include <stdint.h>
+#include <limits.h>
 
 int reverse (int x)
 {
-    int result = 0;
+    int32_t result = 0;
+    //int result = 0;
     int right_num;
 
     while (x != 0)
     {
         right_num = x % 10;
         x = x / 10;
+        if (result > INT_MAX/10) return 0;
+        if (result < INT_MIN/10) return 0;
         result = result*10 + right_num;
     }
     return result;
 
 }
+
 
 int main(){
     
