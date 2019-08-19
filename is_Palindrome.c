0f5c4936438b13b823b151f9724262afe9f0ac4a
Mai Duy Dũng <maiduydung@yahoo.com>
2019-08-19 21:45:48 +0700

    Update is_Palindrome.c

--- is_Palindrome.c
+++ is_Palindrome.c
@@ -3,26 +3,28 @@
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
 bool is_Palindrome(int x){
     if (x < 0)
     {
         return false;
     }
+    if (x > INT_MAX/10 || x < INT_MIN/10) return false; // int overflow check
     int nDigits = floor(log10(abs(x))) + 1;
     char x_str[nDigits];
 
     sprintf(x_str, "%d", x);
     //printf("%s\n",x_str);
 
-    for (int i = 0; i <= strlen(x_str); i++)
+    for (int i = 0; i < strlen(x_str); i++)
     {
-        //printf("%c",x_str[strlen(x_str) - i]);
+        //printf("%c / %d - %c / %ld\n", x_str[i], i, x_str[strlen(x_str) - i - 1], strlen(x_str) - i - 1);
 
-        // if (x_str[i] != x_str[strlen(x_str) - i])
-        // {
-        //     return false;
-        // }
+        if (x_str[i] != x_str[strlen(x_str) - i - 1])
+        {
+            return false;
+        }
         
     }
     return true;
@@ -31,7 +33,7 @@ bool is_Palindrome(int x){
 
 
 int main(){
-    is_Palindrome(12321);
-    //printf("%d",is_Palindrome(12321));
+    //is_Palindrome(12321);
+    printf("%d\n",is_Palindrome(1221));
     return 0;
 }
