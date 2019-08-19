7e934b307aaf5317ddda0c252dcce02eee425197
Mai Duy Dũng <maiduydung@yahoo.com>
2019-08-19 22:53:11 +0700

    Create M_add_two_num.c

---
+++ M_add_two_num.c
@@ -0,0 +1,13 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
+
+}
+
