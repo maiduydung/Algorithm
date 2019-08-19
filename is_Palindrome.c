834431cb1859e250f9cc0520a95d66ab22440c63
Mai Duy Dũng <maiduydung@yahoo.com>
2019-08-19 20:58:23 +0700

    Update is_Palindrome.c

--- is_Palindrome.c
+++ is_Palindrome.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h> 
 #include <math.h>
-#include <limits.h>
+#include <stdlib.h>
+#include <string.h>
 
 bool is_Palindrome(int x){
     if (x < 0)
@@ -11,19 +12,26 @@ bool is_Palindrome(int x){
     int nDigits = floor(log10(abs(x))) + 1;
     char x_str[nDigits];
 
-    const char base_string[] = "base_string";
-    char out_string [MAX_BUFFER_SIZE];
-    int number = 123;
-    sprintf(out_string, "%s%d", base_string, number);
-    printf("out_string = %s\n", out_string);
+    sprintf(x_str, "%d", x);
+    //printf("%s\n",x_str);
 
-    printf("%s",x_str);
+    for (int i = 0; i <= strlen(x_str); i++)
+    {
+        //printf("%c",x_str[strlen(x_str) - i]);
 
+        // if (x_str[i] != x_str[strlen(x_str) - i])
+        // {
+        //     return false;
+        // }
+        
+    }
+    return true;
 }
 
 
 
 int main(){
-    is_Palindrome(1234);
+    is_Palindrome(12321);
+    //printf("%d",is_Palindrome(12321));
     return 0;
 }
