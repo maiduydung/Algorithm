5001f90e8ff468830bb23ab4f2ca808d0e3c261a
Mai Duy Dung <s1f101800698@iniad.org>
2019-09-16 09:05:40 +0900

    Sorting string

---
+++ Sort_string.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void merge(){
+
+}
+
+char *mergesort(char a[], int n){
+    if(n == 1){
+        return a;
+    }
+    //char arr_one[] =  a[0]...a[n/2];
+    for(int i = 0; i <= n/2; i++){
+        
+    }
+}
+
+
+int main(){
+    char my_name[] ="maiduydung";
+    int elements = sizeof(my_name) / sizeof(my_name[0]);
+    mergesort(my_name, elements);
+}
