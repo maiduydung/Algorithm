318dcb7019a6a43bba9d2d9048997b8b358ca140
Mai Duy Dũng <maiduydung@yahoo.com>
2019-09-23 14:47:16 +0900

    Update binary_search_tree.c

--- binary_search_tree.c
+++ binary_search_tree.c
@@ -1,16 +1,47 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct tree
+typedef struct node
 {
     int value;
-    tree *right_node;
-    tree *left_node; 
-} tree;
+    struct node *right_node;
+    struct node *left_node;
+}Node;
 
+Node *init_node(int input_value){
+    Node *temp = (struct node *) malloc(sizeof(struct node));
+    temp ->value = input_value;
+    temp ->left_node = temp ->right_node = NULL;
+    return temp;
+}
 
+Node *insert_node(Node *node, int input_value){
+    if (node == NULL) return init_node(input_value);
+    if (input_value < node ->value)
+        node->left_node = insert_node(node->left_node, input_value);
+    else if(input_value > node ->value)
+        node ->right_node = insert_node(node ->right_node, input_value);
+
+    return node;
+}
+
+void in_order(Node *node){
+    if (node != NULL)
+    {    
+        in_order(node ->left_node);
+        printf("%d\n", node ->value);
+        in_order(node ->right_node);
+    }
+    
+}
 
 int main(){
-    return 1;
-
+    Node *a = NULL;
+    int input_arr[] = {10, 7, 11, 6, 8, 20, 1, 9, 14, 22};
+    for (int i = 0; i < (sizeof input_arr/ sizeof input_arr[0]); i++)
+    {
+        a = insert_node(a, input_arr[i]);
+    }
+    in_order(a);
+    return 0;
 }
