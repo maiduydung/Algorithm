d244b1e1833b109ef33d6328c0c99c1f8f053903
Mai Duy Dũng <maiduydung@yahoo.com>
2019-10-01 14:18:37 +0900

    Update binary_search_tree.c

--- binary_search_tree.c
+++ binary_search_tree.c
@@ -25,14 +25,23 @@ Node *insert_node(Node *node, int input_value){
     return node;
 }
 
+void in_order_sort(Node *node, char result){
+    if (node != NULL)
+    {    
+        in_order_sort(node ->left_node);
+        
+        printf("%c\n", node ->value);
+        in_order_sort(node ->right_node);
+    }
+}
+
 void in_order(Node *node){
     if (node != NULL)
     {    
         in_order(node ->left_node);
-        printf("%d\n", node ->value);
+        printf("%c\n", node ->value);
         in_order(node ->right_node);
     }
-    
 }
 
 void pre_order(Node *node){
@@ -56,11 +65,14 @@ void post_order(Node *node){
 
 int main(){
     Node *a = NULL;
-    int input_arr[] = {10, 7, 11, 6, 8, 20, 1, 9, 14, 22};
+    char input_arr[] = "maiduydung";
+    int size_n = sizeof input_arr / sizeof input_arr[0];
+    char sorted_arr [size_n];
+
     for (int i = 0; i < (sizeof input_arr/ sizeof input_arr[0]); i++)
     {
         a = insert_node(a, input_arr[i]);
     }
-    post_order(a);
+    in_order(a);
     return 0;
 }
