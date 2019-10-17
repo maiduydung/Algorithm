b2f151de229574227f895a7f571e80d587eac11d
Mai Duy Dũng <maiduydung@yahoo.com>
2019-10-18 05:39:34 +0900

    Update memoization_fib.c

--- memoization_fib.c
+++ memoization_fib.c
@@ -1,17 +1,2 @@
 #include <stdio.h>
 #include <malloc.h>
-int fib(int n, int checked[]){
-    if (n <= 1) return n;
-    if (checked[n]) return n;
-    else{
-        checked[n] = fib(n-1, checked) + fib(n-2, checked);
-        return checked[n];
-    }
-}
-
-int main(){
-    int n = 100;
-    
-
-    return 0;
-}
