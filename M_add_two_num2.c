86755ffc29e7c8d2dff54516d52b92912ca55291
Maiduydung <maiduydung@yahoo.com>
2020-02-18 20:35:21 +0900

    new add 2 num excercise on leetcode

---
+++ M_add_two_num2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+ 
+
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
+
+}
+
+int main(){
+    return 0;
+}
