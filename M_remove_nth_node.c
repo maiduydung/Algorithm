2b9aec6e81510de4fa674ee26539cafcaa5e9a1e
DUNG MAI DUY <s1f101800698@iniad.org>
2020-02-20 16:48:37 +0900

    leetcode #19 finally done

--- M_remove_nth_node.c
+++ M_remove_nth_node.c
@@ -12,60 +12,31 @@ struct ListNode{
 
 
 
-struct ListNode *reverse(struct ListNode *head, int* len){
-    struct ListNode *curr = head;
-    struct ListNode *prev = NULL;
-    struct ListNode *next = NULL;
 
-    while (curr != NULL)
-    {
-        next = curr ->next;
-        curr ->next = prev;
-        prev = curr;
-        curr = next;
-        *len = *len + 1;
-       
-    }
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n){   
+    struct ListNode* fast = head;
+    struct ListNode* slow = head;
     
-    head = prev;
+    for(int i = 0; i < n; i++){
+        //printf("%d \n", fast ->val);
+        fast = fast ->next;
+    }
+    if (fast == NULL){
+        printf("head %d", head->val);
+        return head->next;
+    }
+        
+    
+        
 
+    while (fast->next != NULL) 
+    {
+        fast = fast->next;
+        slow = slow->next;
+    }
+    slow ->next = slow -> next -> next;
     return head;
 }
-
-
-struct ListNode* removeNthFromEnd(struct ListNode* head, int n){
-    int len;
-    head = reverse(head, &len);
-    struct ListNode *curr = head;
-    struct ListNode *prev = NULL;
-    
-    int i = 1;
-    printf("length %d", len);
-
-    if(len == 1)
-    {
-        struct ListNode *empty = NULL;
-        head = empty;
-    }
-    
-    while (curr != NULL)
-    {        
-        if (i == n)
-        {
-            prev ->next = curr ->next;
-            curr ->next = NULL;
-        }
-
-        prev = curr;
-        curr = curr ->next;
-        i++;
-    }
-    head = reverse(head, &len);
-
-
-    return head;
-}
-
 int main(){
     return 0;
 }
