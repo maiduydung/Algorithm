8c34a95c05a47027b3fe357c985e42b9f52853f9
maiduydung <s1f101800698@iniad.org>
2020-03-13 20:30:52 +0900

    smaller than current element in array check

--- M_subarray.py
+++ M_subarray.py
@@ -1,18 +1,23 @@
 # https://leetcode.com/problems/subarray-sum-equals-k/submissions/
-# Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+# Given an array of integers and an integer k, 
+# you need to find the total number of continuous subarrays whose sum equals to k.
 
 # Example 1:
-# Input:nums = [1,1,1], k = 2
-# Output: 2
+# Input:nums = [1,2,1,1,1], k = 3
+# Output: 3 
+# [1,2] [2,1] [1,1,1]
 
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        first = 0
-        second = 0
-        temp = 0
-        result = 0
+        sub_arr_cnt = 0
+        dict_set = {0:1}
+        sum = 0
         for i in range(len(nums)):
-            temp = temp + nums[i]
-            first = first + 1
-            if(temp == k):
-                result = result + 1
+            sum = sum + nums[i]
+            if (sum - k) in dict_set:
+                sub_arr_cnt += dict_set[sum - k]
+            if sum not in dict_set:
+                dict_set[sum] = 1
+            else:
+                dict_set[sum] += 1
+        return sub_arr_cnt
