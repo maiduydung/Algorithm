8127dea2025e868254e84ed5d692c82d85f74982
maiduydung <s1f101800698@iniad.org>
2020-03-19 16:34:47 +0900

    edit knightsTour

--- Backtracking/BCKNIGHT.py
+++ Backtracking/BCKNIGHT.py
@@ -12,21 +12,44 @@
 
 # valid move |X-Y| <=3
 
-def BCKNIGHT(SIZE: int, X_in: int, Y_in: int):
-    count = 0
-    result = [[0]]
-    checked = set()
+class KnightsTour:
+    def __init__(self, size_in, x_in, y_in):
+        self.X = x_in
+        self.Y = y_in
+        self.Size = size_in
+        self.squares = size_in * size_in 
+        self.board = [[0 for i in range(self.Size)] for j in range(self.Size)]
+        self.count = 0
+        self.x_offset = [-2,-2,-1,-1, 1, 1, 2, 2]
+        self.y_offset = [-1, 1,-2, 2,-2, 2,-1, 1]
+        
+    def print_board(self):
+        #print(self.board)
+        for i in range(self.Size):
+            for j in range(self.Size):
+                print(self.board[i][j], end=' ')
+            print('')
 
-    board = [[0 for x_size in range(SIZE)] for y_size in range(SIZE)]
-    print(board)
+    def move(self, x:int, y:int):
+        self.count = self.count + 1
+        self.board[x][y] = self.count
+        for i in range(8):
+            if(self.count == self.Size^2):
+                #print(self.board)
+                return self.board
+            u = x + self.x_offset[i]
+            v = y + self.y_offset[i]
+            if(u >= 0) and (u < self.Size) and (v >= 0) and (v < self.Size) and (self.board[u][v] == 0):
+                self.print_board()
+                self.move(u, v)
+            #board[u][v] = 0 => not travel there yet
+        
+        self.count = self.count - 1    
+        self.board[x][y] = 0
 
-    if count == SIZE^2:
-        return result
-    else:
-        for x in range(X_in,SIZE):
-            for y in range(Y_in ,SIZE):
-                if(abs(x - y) <= 3):
-                    print(board[x][y], x, y)
-                # if(abs(X))
 
-BCKNIGHT(4,1,2)
+    
+
+test = KnightsTour(5, 0 ,0)
+test.move(0, 0)
+test.print_board()
