12ff99247fffaf515e7af95a6d73abb8d03ac8c5
maiduydung <s1f101800698@iniad.org>
2020-04-02 13:00:58 +0900

    merging 2 binary trees with inorder traversal

---
+++ Tree/mergeTrees.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/merge-two-binary-trees/
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if(!t1) return t2;
+        if(!t2) return t1;
+        
+        t1 ->left = mergeTrees(t1->left, t2->left);
+        t1 ->val = t1->val + t2->val;
+        t1 ->right = mergeTrees(t1->right, t2->right);
+        return t1;
+    }
+};
