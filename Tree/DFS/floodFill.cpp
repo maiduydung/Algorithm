b1ff63c60662004a3f4841dc34418e4de1ce209d
Mai Duy Dũng <maiduydung@yahoo.com>
2020-04-03 22:36:05 +0900

    edit floodfill.cpp
    added
    (image[sr][sc] != oldColor)

--- Tree/DFS/floodFill.cpp
+++ Tree/DFS/floodFill.cpp
@@ -1,8 +1,27 @@
 // https://leetcode.com/problems/flood-fill/
+#include <iostream>
+#include <vector>
 
+using namespace std;
 class Solution {
-public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        
+private:
+    void dfs(vector<vector<int>>& image, int sr, int sc, int oldColor ,int newColor){
+        //boundary check
+        if((sr < 0) || (sc < 0) || (sr >= image.size()) || (sc >= image[0].size()) || (image[sr][sc] != oldColor))//making sure the color change is only applied to those with the same color
+            return;
+        image[sr][sc] = newColor;
+        dfs(image, sr + 1, sc, oldColor, newColor );
+        dfs(image, sr, sc + 1, oldColor, newColor );
+        dfs(image, sr - 1, sc, oldColor, newColor );
+        dfs(image, sr, sc - 1, oldColor, newColor );
     }
+public:
+
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        int oldColor = image[sr][sc];
+        if(oldColor != newColor)
+            dfs(image, sr, sc, image[sr][sc], newColor);
+        return image;
+    }
+
 };
