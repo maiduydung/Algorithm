366f373eae504f58eddd0f4dffa520ae5b6b2a2a
Mai Duy Dũng <maiduydung@yahoo.com>
2020-04-26 18:09:07 +0900

    stack with O(1) time

--- Stack/minStack.cpp
+++ Stack/minStack.cpp
@@ -18,32 +18,35 @@
 // minStack.pop();
 // minStack.top();    // return 0
 // minStack.getMin(); // return -2
-#include<vector>
+
+//Methods pop, top and getMin operations will always be called on non-empty stacks.
+#include<stack>
 using namespace std;
 
 class MinStack {
-    private:
-        vector<int> MyStack={};
-        int min_val = INT_MAX;
-    public:
-        void push(int x) {
-            MyStack.push_back(x);
-        }
-        
-        void pop() {
-            MyStack.pop_back();
-        }
-        
-        int top() {
-            return MyStack[MyStack.size()-1];
-        }
-        
-        int getMin() {
-            //for(auto num:MyStack)
-            //    cout << num << endl;
-            for(int current:MyStack)
-                if(current < min_val)
-                    min_val = current;
-            return min_val;
-        }
+private:
+    stack<int> stk1;
+    stack<int> stk2;
+public:   
+    void push(int x) {
+        stk1.push(x);
+        //there may be multiple equal value so x <= stk2.top()
+        //when we pop those value, its remaining min still remains
+        if(stk2.empty() || x <= stk2.top()) 
+            stk2.push(x);            
+    }
+    
+    void pop() {
+        if(stk1.top() == stk2.top())
+            stk2.pop();
+        stk1.pop();
+    }
+    
+    int top() {
+        return stk1.top();
+    }
+    
+    int getMin() {
+        return stk2.top();
+    }
 };
