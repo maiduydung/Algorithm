7bdf9397637e041f06d9c69368279040b1795367
Mai Duy Dũng <maiduydung@yahoo.com>
2020-05-14 23:48:39 +0900

    edit to O(n^e)

--- Pathfinding/dijkstra.py
+++ Pathfinding/dijkstra.py
@@ -3,12 +3,6 @@ import matplotlib.pyplot as plt
 
 G = nx.read_weighted_edgelist('dij.edgelist', nodetype = int)
 
-# D = [float('inf')] * nx.number_of_nodes(G)
-# Unvisited = []
-# for i in G.nodes():
-#     Unvisited.append(i)
-
-#print(G.edges[0, 1]['weight'])
 
 def get_min_node(D, X):
     arg_min = -1
@@ -26,27 +20,20 @@ def print_sol(D):
 
 def dijkstra(G, source):
     D = [float('inf')] * nx.number_of_nodes(G)
-    D_cal = [float('inf')] * nx.number_of_nodes(G)
-    X = []
-    for i in G.nodes():
-        X.append(i)
     D[source] = 0
-    X.remove(source)
-
-    print("all nodes: ", G.nodes())
+    X = set(G.nodes())
     while X:
-        for node in G.nodes():
-            for i in G.neighbors(node):
-                D_cal[i] = G.edges[node, i]['weight'] + D[node]
-                min_node = get_min_node(D_cal, X)
-                if min_node in X:
-                    if (D[i] > D_cal[i]):
-                        D[i] = D_cal[i]
-                        X.remove(min_node)
+        min_node = get_min_node(D, X)
+        X.remove(min_node)
+        neighbors = G.neighbors(min_node)
+        for neighbor in neighbors:
+            if neighbor in X:
+                if(D[min_node] + G.edges[min_node, neighbor]['weight'] < D[neighbor]):
+                    D[neighbor] = (D[min_node] + G.edges[min_node, neighbor]['weight'])
+    
 
-    for i in range(len(D)):
-        print(i, " ", D[i])
+    
+    print(D)
 
-    print('---------')
-    print(nx.shortest_path(G,source))
-dijkstra(G,1)
+dijkstra(G,0)
+
