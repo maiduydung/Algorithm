6d503289af930ddf85e128d0acf0d5147b95ce7c
Mai Duy Dũng <maiduydung@yahoo.com>
2020-05-20 19:33:23 +0900

    not there yet

--- Pathfinding/dijkstra_rand_graph.py
+++ Pathfinding/dijkstra_rand_graph.py
@@ -1,19 +1,10 @@
 import networkx as nx
 import time
+import heapq
 
 
-G = nx.read_weighted_edgelist('random_graph.edgelist', nodetype = int)
+G = nx.read_weighted_edgelist('dij.edgelist', nodetype = int)
 
-
-def get_min_node(D, X):
-    arg_min = -1
-    min_val = float('inf')
-    for i in range(len(D)):
-        if D[i] < min_val:
-            if i in X:
-                arg_min = i
-                min_val = D[i]
-    return arg_min
 
 def print_sol(D):
     for i in range(len(D)):
@@ -23,15 +14,26 @@ def dijkstra(G, source):
     D = [float('inf')] * nx.number_of_nodes(G)
     D[source] = 0
     X = set(G.nodes())
+    heap = []
+    heapq.heappush(heap,(0,source))
+
     while X:
-        min_node = get_min_node(D, X)
+        #min_node = get_min_node(D, X)
+        min_node, min_idx = heapq.heappop(heap)
+        #if overlapped entry for an node
+
+        if(heap):
+            print(heap[0])
+            if(heap[0][0] > min_node):
+                heapq.heappop(heap)
+
         X.remove(min_node)
         neighbors = G.neighbors(min_node)
         for neighbor in neighbors:
             if neighbor in X:
+                heapq.heappush(heap,(neighbor, D[neighbor]))
                 if(D[min_node] + G.edges[min_node, neighbor]['weight'] < D[neighbor]):
                     D[neighbor] = (D[min_node] + G.edges[min_node, neighbor]['weight'])
-    
 
     
     print(D)
@@ -39,4 +41,6 @@ start = time.time()
 dijkstra(G,0)
 
 print("Elapsed time n = 10000: ", time.time() - start)
+
+
 
