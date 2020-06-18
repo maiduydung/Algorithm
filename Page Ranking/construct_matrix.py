c379323cf5ded37bb06153a87d89423ac0343e3c
Mai Duy Dũng <maiduydung@yahoo.com>
2020-06-18 21:36:17 +0900

    multiply matrix with importance vetor

--- Page Ranking/construct_matrix.py
+++ Page Ranking/construct_matrix.py
@@ -4,9 +4,19 @@ G = nx.read_edgelist('pr.edgelist', create_using = nx.DiGraph(), nodetype = int)
 
 def matrix(G):
     n = nx.number_of_nodes(G)
+    I_v = np.array([[1/n]*n])
+    I_v = I_v.transpose()
+    #print(I_v.shape)
     A = nx.adjacency_matrix(G, nodelist=range(n), weight=None)
     A = A.todense()
     A = A.astype(float)
-    print(A)
+    
+    A = A.transpose()
+    cols_sum = A.sum(axis = 0)
+    A[:,0] = A[:, 0] / cols_sum[0,0]
+    A[:, 1] = np.ones((n,1))
+
+    print(np.matmul(A,I_v))
+    
 
 matrix(G)
