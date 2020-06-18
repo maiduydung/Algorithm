6c29cc5eeace04f743fa0562662262f21acafbf1
Mai Duy Dũng <maiduydung@yahoo.com>
2020-06-18 23:53:28 +0900

    update with matrix multiplication

--- Page Ranking/construct_matrix.py
+++ Page Ranking/construct_matrix.py
@@ -4,19 +4,31 @@ G = nx.read_edgelist('pr.edgelist', create_using = nx.DiGraph(), nodetype = int)
 
 def matrix(G):
     n = nx.number_of_nodes(G)
-    I_v = np.array([[1/n]*n])
-    I_v = I_v.transpose()
-    #print(I_v.shape)
     A = nx.adjacency_matrix(G, nodelist=range(n), weight=None)
     A = A.todense()
     A = A.astype(float)
     
     A = A.transpose()
     cols_sum = A.sum(axis = 0)
-    A[:,0] = A[:, 0] / cols_sum[0,0]
-    A[:, 1] = np.ones((n,1))
+    A = A / cols_sum
+    A[np.isnan(A)] = 1/n
 
-    print(np.matmul(A,I_v))
-    
+    return(A)
 
-matrix(G)
+def update(G,v,d,k):
+    A = matrix(G)
+    result = v
+    n = nx.number_of_nodes(G)
+    J = np.ones((n,n))/n
+    M_G = A*d + (1-d)*J
+    for i in range(k):
+        result = np.matmul(M_G,result)
+    return result
+
+v = np.ones((12,1))/12
+print(update(G,v,0.85,1000))
+
+pr = nx.pagerank(G, alpha = 0.85)
+print(pr)
+
+#matrix(G)
