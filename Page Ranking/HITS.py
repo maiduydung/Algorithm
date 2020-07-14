187bf65813cbe13e8f1d4370b42d5cad101998e5
Mai Duy Dũng <maiduydung@yahoo.com>
2020-07-15 00:55:53 +0900

    actually we just need to transpose the marix here, no need for normalization

--- Page Ranking/HITS.py
+++ Page Ranking/HITS.py
@@ -3,19 +3,18 @@ import numpy as np
 
 G = nx.read_edgelist("pr.edgelist", create_using = nx.DiGraph(), nodetype = int)
 
-def transition_matrix(G):
+def transpose_matrix(G):
     n = nx.number_of_nodes(G)
     A = nx.adjacency_matrix(G, nodelist=range(n), weight="weight")
     A = A.todense()
     A = A.astype(float)
     A = A.transpose()
-    col_sum = A.sum(axis = 0)
-    for i in range(n):
-        s = col_sum[0, i]
-        if (s != 0):
-            A[:,i] = A[:,i] / s
-        else:
-            A[:,i] = 1/n
     return A
 
-print(transition_matrix(G))
+
+h0 = np.zeros((n ,1))
+
+
+def update(G, h0, k):
+    for _ in range(k):
+        
