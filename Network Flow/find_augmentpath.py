33d7712a0c8798e73703c1861093c65e4ee3ba53
Mai Duy Dũng <maiduydung@yahoo.com>
2020-07-15 21:17:37 +0900

    rewrite

--- Network Flow/find_augmentpath.py
+++ Network Flow/find_augmentpath.py
@@ -4,22 +4,22 @@ import networkx as nx
 import queue
 N = nx.read_weighted_edgelist('ff.edgelist', nodetype = int)
 
-
-def find_augmentpath(N, s, t):
-    P = [s] * nx.number_of_nodes(N)
+def find_augmentpath(G, s, t): #graph, source, sink
+    P = [s] * nx.number_of_nodes(G)
     visited = set()
     stack = queue.LifoQueue()
     stack.put(s)
-    while (not stack.empty()):
-        v = stack.get()
+    while(not stack.empty()):
+        v = stack.get() #pop stack
         if v == t:
-            return (P,True)
+            return (P, True)
         if not v in visited:
-            visited.add(v)
-            for w in N.neighbors(v):
-                if (w not in visited and N.edges[v,w]['weight'] > 0):
+            stack.put(v)
+            for w in G.neighbors(v):
+                if (not w in visited) and (G.edges[v,w]['weight'] > 0):
                     stack.put(w)
-                    P[w] = v
+                    P[w] = v # previous node of w is v
+
     return (P, False)
 
 print(find_augmentpath(N, 0, 5))
